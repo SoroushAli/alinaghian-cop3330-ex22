4a21cf459bb39b74a5e773ea567f15b82b296d4b
SoroushAli <84461814+SoroushAli@users.noreply.github.com>
2021-06-06 08:27:31 -0400

    Solution for exercise 22

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -3,6 +3,9 @@
  *  Copyright 2021 Soroush Alinaghian
  */
 package base;
+
+import java.util.Scanner;
+
 /*
 Comparing one input to a known value is common enough, but you’ll often need to process a collection of inputs.
 
@@ -25,7 +28,37 @@ Modify the program so that it asks for ten numbers instead of three.
 Modify the program so that it asks for an unlimited number of numbers.
  */
 public class App {
+    static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+        int firstNum = getFirstNum();
+        int secondNum = getSecondNum();
+        int thirdNum = getThirdNum();
+        findLargestNum(firstNum, secondNum, thirdNum);
+    }
+
+    private static void findLargestNum(int first, int sec, int third) {
+        int numbers[] = {first, sec, third};
+        int max = numbers[0];
+        for (int i = 0; i < 3; i++) {
+            if(numbers[i] > max)
+                max = numbers[i];
+        }
+        System.out.println(String.format("The largest number is %d.", max));
+    }
+
+    private static int getFirstNum() {
+        System.out.print("Enter the first number: ");
+        return in.nextInt();
+    }
+
+    private static int getSecondNum() {
+        System.out.print("Enter the second number: ");
+        return in.nextInt();
+    }
+
+    private static int getThirdNum() {
+        System.out.print("Enter the third number: ");
+        return in.nextInt();
     }
 }
